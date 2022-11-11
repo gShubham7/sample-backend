fff4d70ad5e93de2cab6dfe46b2cc0e55aa3764c
shubham <gaikwad7777@gmail.com>
2022-11-12 01:50:38 +0530

    backend added

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+require("dotenv").config();
+const connect = require("./config/db");
+const cors = require("cors");
+const PORT = process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(cors()); /// Ye File Upar Rahengi
+
+
+const UserRoute = require("./features/user/user.router");
+const MenRoute = require("./features/mens/mens.router");
+const WomenRoute = require("./features/womens/womens.router");
+const ElectronicRoute = require("./features/electronics/electronics.router");
+const CartRoute = require("./features/cart/cart.router");
+app.use("/user", UserRoute);
+app.use("/mens", MenRoute);
+app.use("/womens", WomenRoute);
+app.use("/electronics", ElectronicRoute);
+app.use("/cart", CartRoute);
+
+
+app.listen(PORT, async () => {
+  await connect();
+  console.log(`listen at http://localhost:${PORT}`);
+});
