6cd118b9511613588e82c06bf7058d4f8eac9242
shubham <gaikwad7777@gmail.com>
2022-11-12 12:03:41 +0530

    added

--- src/server.js
+++ src/server.js
@@ -8,18 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(cors()); /// Ye File Upar Rahengi
 
-
 const UserRoute = require("./features/user/user.router");
 const MenRoute = require("./features/mens/mens.router");
 const WomenRoute = require("./features/womens/womens.router");
 const ElectronicRoute = require("./features/electronics/electronics.router");
 const CartRoute = require("./features/cart/cart.router");
+app.get("/", async (req, res) => {
+  res.send("This is homepage");
+});
 app.use("/user", UserRoute);
 app.use("/mens", MenRoute);
 app.use("/womens", WomenRoute);
 app.use("/electronics", ElectronicRoute);
 app.use("/cart", CartRoute);
-
 
 app.listen(PORT, async () => {
   await connect();
